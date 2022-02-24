b38062ef30a447ea063fc5ebdcb0a6f3e7a0720a
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2022-02-24 08:57:08 +0300

    classwork

---
+++ in-class/Project2/Project2/main.cpp
@@ -0,0 +1,21 @@
+#include "Class.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	Product prod1, prod2("1", "Product1", 10, 3, 100);
+
+	if (prod1 == prod2) {
+
+	}
+
+	int a, b;
+
+	if (a == b) {
+
+	}
+
+	system("pause");
+	return 0;
+}
