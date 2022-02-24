c82f943e3fbbc8b3157b2f7ab04c0a33ea5c2529
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2022-02-24 17:46:25 +0300

    lab 1 final ver

--- Homework/HW-1/main.cpp
+++ Homework/HW-1/main.cpp
@@ -4,33 +4,44 @@
 using namespace std;
 
 int main() {
-	std::string S;
+	std::string timeString1, timeString2;
+	
+	cout << "enter time string" << endl;
+	cin >> timeString1;
 
-	std::cout << "Enter time: ";
-
-	std::cin >> S;
-
-	Time time_0;
-	Time time_1(S);
-	Time time_2(12, 12, 12);
-	Time time_3(time_1);
-	Time time_4;
+	Time time_0, time_1, time_6;
+	Time time_2(12, 23, 34);
+	Time time_3(time_2);
+	Time time_4(timeString1);
 	Time time_5;
+	
+	cout << time_0 << endl;
 
-	time_0.timePrint();
-	time_1.timePrint();
-	time_2.timePrint();
-	time_3.timePrint();
-	time_4.timePrint();
-	time_5.timePrint();
+	cout << "enter time, hour - minute - second" << endl;
+	cin >> time_1;
 
-	time_0.timeOperation(160, '-');
-	time_0.timePrint();
+	time_5 = time_2;
 
-	time_5.timeOperation(5,20,30);
-	time_5.timePrint();
+	cout << "add and substract" << endl;
+	time_2 + 26;
+	cout << time_2 << endl;
+	time_5 - 35;
+	cout << time_5 << endl;
 
-	time_0.timeOperation(time_4);
+	cout << time_4 << endl;
 
+	cout << "time to string" << endl;
+	timeString2 = time_2.timeToString(timeString2);
+	cout << timeString2 << endl;
+
+	cout << time_3 << endl;
+
+	if (time_2 != time_0)
+		cout << "test 1" << endl;
+	if (time_0 == time_6)
+		cout << "test 2" << endl;
+
+
+	system("pause");
 	return 0;
 }
--- Homework/HW-1/timetsk.h
+++ Homework/HW-1/timetsk.h
@@ -6,14 +6,88 @@ class Time {
 private:
 	int hou, min, sec;
 public:
+	// все необходимые конструкторы
 	Time();
 	Time(int _hou, int _min, int _sec);
 	Time(const Time &t);
-	Time(std::string str_time);
+	Time(std::string str_time); // преобразование времени из строки
 	~Time();
 
-	void timePrint();
-	void timeOperation(const Time &t);
-	void timeOperation(int sec_, char op);
-	void timeOperation(int hou_, int min_, int sec_);
+	
+
+	std::string timeToString(std::string _string); // метод представления времени как строки
+
+
+	// перегрузки перегрузочки 
+	
+	void operator=(const Time& t) {
+		this->hou = t.hou;
+		this->min = t.min;
+		this->sec = t.sec;
+	};
+
+	bool operator==(const Time& t) {
+		int totSec1 = ((this->hou * 60 + this->min) * 60 + this->sec);
+		int totSec2 = ((t.hou * 60 + t.min) * 60 + t.sec);
+		return (totSec1 == totSec2);
+	};
+
+	bool operator!=(const Time& t) {
+		int totSec1 = ((this->hou * 60 + this->min) * 60 + this->sec);
+		int totSec2 = ((t.hou * 60 + t.min) * 60 + t.sec);
+		return (totSec1 != totSec2);
+	};
+
+	void operator+(int _sec) {
+		int totSecBfr = ((this->hou * 60 + this->min) * 60 + this->sec);
+		int totSecAfr = totSecBfr + _sec;
+		this->hou = (totSecAfr / 3600);
+		this->min = ((totSecAfr / 60) - (this->hou * 60));
+		this->sec = (totSecAfr - ((this->min * 60) + (this->hou * 3600)));
+		if (this->hou == 24)
+			this->hou = 0;
+		if (this->min == 60)
+			this->min = 0;
+		if (this->sec == 60)
+			this->sec = 0;
+	};
+
+	void operator-(int _sec) {
+		int totSecBfr = ((this->hou * 60 + this->min) * 60 + this->sec);
+		int totSecAfr = totSecBfr - _sec;
+		this->hou = (totSecAfr / 3600);
+		this->min = ((totSecAfr / 60) - (this->hou * 60));
+		this->sec = (totSecAfr - ((this->min * 60) + (this->hou * 3600)));
+		if (this->hou == 24)
+			this->hou = 0;
+		if (this->min == 60)
+			this->min = 0;
+		if (this->sec == 60)
+			this->sec = 0;
+	};
+
+	friend std::ostream& operator<< (std::ostream &out, const Time& t) {
+		std::string hous = std::to_string(t.hou);
+		std::string mins = std::to_string(t.min);
+		std::string secs = std::to_string(t.sec);
+		if (t.hou < 10 && t.hou > -10) {
+			hous = "0" + hous;
+		}
+		if (t.min < 10 && t.min > -10) {
+			mins = "0" + mins;
+		}
+		if (t.sec < 10 && t.sec > -10) {
+			secs = "0" + secs;
+		}
+		out << "Current time is: " << hous << ":" << mins << ":" << secs;
+		return out;
+	};
+
+	friend std::istream& operator>> (std::istream &in, Time& t) {
+		in >> t.hou;
+		in >> t.min;
+		in >> t.sec;
+
+		return in;
+	};
 };
