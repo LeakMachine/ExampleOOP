32541bdf6fdb936d1119e05d632a09b7de6c6487
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2022-03-17 12:21:07 +0300

    1. functions

--- Classwork/Classwork/Classwork-1/main.cpp
+++ Classwork/Classwork/Classwork-1/main.cpp
@@ -74,13 +74,171 @@ void inputTriangle(Type _type) {
 
 */
 
+
+void userPick1(int* chosen) {
+    std::cout << "Ways to define a triangle:" << std::endl
+        << "1) by three sides," << std::endl
+        << "2) by base and height," << std::endl
+        << "3) by three points" << std::endl
+        << "Input: ";
+    std::cin >> (*chosen);
+};
+
+void userPick2(int* chosen) {
+    std::cout << "Methods: 1) get square," << std::endl
+        << "2) get perimeter, " << std::endl
+        << "3) get type of triangle," << std::endl
+        << "4) compare with another triangle by square," << std::endl
+        << "5) back to first menu," << std::endl
+        << "0) exit," << std::endl
+        << "Input: ";
+    std::cin >> (*chosen);
+};
+
+void inputBaseH(int* base, int* height) {
+    int AB = (*base), h = (*height);
+    std::cout << "Input base of triangle: ";
+    std::cin >> AB;
+    std::cout << "Input height of triangle: ";
+    std::cin >> h;
+    (*base) = AB; (*height) = h;
+};
+
+void inputSides(int* side1, int* side2, int* side3) {
+    int AB = (*side1), AC = (*side2), BC = (*side3);
+    while (1) {
+        std::cout << "Input 3 side of triangle (between space): ";
+        std::cin >> AB >> AC >> BC;
+        if (!(AB + BC > AC && AB + AC > BC && AC + BC > AB)) {
+            std::cout << "Input error: triangle dont exist" << std::endl;
+            std::cout << "Try again" << std::endl;
+            continue;
+        }
+        (*side1) = AB; (*side2) = AC; (*side3) = BC;
+        break;
+    }
+};
+
+void inputCoordinates(int coord1[], int coord2[], int coord3[]) {
+    std::cout << "Input 1st point's coordinates (between space): ";
+    std::cin >> coord1[0] >> coord1[1];
+    std::cout << "Input 2nd point's coordinates (between space): ";
+    std::cin >> coord2[0] >> coord2[1];
+    std::cout << "Input 3rd point's coordinates (between space): ";
+    std::cin >> coord3[0] >> coord3[1];
+};
+
+void calcSqSides(int side1, int side2, int side3) {
+    int AB = side1; int AC = side2; int BC = side3; float S; float p;
+    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
+        p = (AB + BC + AC) * 0.5;
+        S = sqrt(p * (p - AB) * (p - BC) * (p - AC));
+        std::cout << "Square of triangle: S = " << S << std::endl;
+    }
+    else {
+        std::cout << "Input error: triangle dont exist" << std::endl;
+    }
+};
+
+void calcSqBaseH(int side1, int height) {
+    int AB = side1; int h = height; float S;
+    if (h != 0) {
+        S = 0.5 * AB * h;
+        std::cout << "Square of triangle: S = " << S << std::endl;
+    }
+    else {
+        std::cout << "Input error: triangle dont exist" << std::endl;
+    }
+};
+
+void calcSqPoints(int coord1[], int coord2[], int coord3[]) {
+    float S;
+    S = 0.5 * abs(((coord1[0] - coord3[0]) * (coord2[1] - coord3[1])) - ((coord2[0] - coord3[0]) * (coord1[1] - coord3[1])));
+    std::cout << "Square of triangle: S = " << S << std::endl;
+};
+
+void calcPrSides(int side1, int side2, int side3) {
+    int AB = side1; int AC = side2; int BC = side3; float p;
+    p = AB + BC + BC;
+    std::cout << "Perimeter: " << p << std::endl;
+}
+
+void calcPrBaseH() {
+    std::cout << "Input error: we couldn't find perimeter by your parameters" << std::endl;
+};
+
+void calcPrPoints(int coord1[], int coord2[], int coord3[]) {
+    float p;
+    int ABx = coord2[0] - coord1[0];
+    int ABy = coord2[1] - coord1[1];
+    int ACx = coord3[0] - coord1[0];
+    int ACy = coord3[1] - coord1[1];
+    int BCx = coord3[0] - coord2[0];
+    int BCy = coord3[1] - coord2[1];
+    p = sqrt(pow(ABx, 2) + pow(ABy, 2)) + sqrt(pow(ACx, 2) + pow(ACy, 2)) + sqrt(pow(BCx, 2) + pow(BCy, 2));
+    std::cout << "Perimeter: " << p << std::endl;
+};
+
+void triangleType(int side1, int side2, int side3) {
+    int AB = side1; int AC = side2; int BC = side3;
+    if (AB == AC == BC) {
+        std::cout << "Triangle is equilateral" << std::endl;
+    }
+    else if (AB == AC || AC == BC || AB == BC && !(AB == AC == BC)) {
+        std::cout << "Triangle is isosceles" << std::endl;
+    }
+    else if (pow(AB, 2) + pow(AC, 2) == pow(BC, 2) || pow(BC, 2) + pow(AC, 2) == pow(AB, 2) || pow(AB, 2) + pow(BC, 2) == pow(AC, 2)) {
+        std::cout << "Triangle is isosceles" << std::endl;
+    }
+    else {
+        std::cout << "Triangle is scalene" << std::endl;
+    }
+};
+
+void triangleCompare(float S1, float S2) {
+    if (S1 == S2)
+        std::cout << "Triangles have equal square" << std::endl;
+    else
+        std::cout << "Triangles' squares are not equal" << std::endl;
+};
+
 enum Type { bySides, byBaseAndHeight, byPointsCoords };
 
+void inputTriangle(Type _type) {
+    switch (_type) {
+    case bySides:
+        break;
+    case byBaseAndHeight:
+        break;
+    case byPointsCoords:
+        break;
+    }
+};
+
+void menuExitOrBack(int side1, int side2, int side3, int coord1[], int coord2[], int coord3[], int height, Type input) {
+    system("cls");
+    int AB = side1; int AC = side2; int BC = side3; int h = height;
+    std::cout << "Last parameters: ";
+    if (input == bySides) {
+        std::cout << AB << "x" << BC << "x" << AC << std::endl;
+    }
+    else if (input == byBaseAndHeight) {
+        std::cout << "a = " << AB << ", h = " << h << std::endl;
+    }
+    else if (input == byPointsCoords) {
+        std::cout << "(" << coord1[0] << ", " << coord1[1] << ")" << ", ";
+        std::cout << "(" << coord2[0] << ", " << coord2[1] << ")" << ", ";
+        std::cout << "(" << coord3[0] << ", " << coord3[1] << ")" << std::endl;
+    }
+};
+
+
 int main() {
+    int S1 = 0, S2 = 0;
     int user_choosen;
     int AB, BC, AC;
     int A[2], B[2], C[2];
-    int h;
+    int h = 0;
     float p, S;
     bool exit_flag = false, back_flag;
 
@@ -88,122 +246,59 @@ int main() {
 
     while (!exit_flag) {
         back_flag = false;
-        std::cout << "Ways to define a triangle:" << std::endl
-            << "1) by three sides," << std::endl
-            << "2) by base and height," << std::endl
-            << "3) by three points" << std::endl
-            << "Input: ";
-        std::cin >> user_choosen;
-
+        userPick1(&user_choosen);
         switch (user_choosen) {
         case 1:
             _inputType = bySides;
-            while (1) {
-                std::cout << "Input 3 side of triangle (between space): ";
-                std::cin >> AB >> BC >> AC;
-                if (!(AB + BC > AC && AB + AC > BC && AC + BC > AB)) {
-                    std::cout << "Input error: triangle dont exist" << std::endl;
-                    std::cout << "Try again" << std::endl;
-                    continue;
-                }
-                break;
-            }
+            inputSides(&AB, &AC, &BC);
             break;
         case 2:
             _inputType = byBaseAndHeight;
-            std::cout << "Input base of triangle: ";
-            std::cin >> AB;
-            std::cout << "Input height of triangle: ";
-            std::cin >> h;
+            inputBaseH(&AB, &h);
             break;
         case 3:
             _inputType = byPointsCoords;
-            std::cout << "Input 1st point's coordinates (between space): ";
-            std::cin >> A[0] >> A[1];
-            std::cout << "Input 2nd point's coordinates (between space): ";
-            std::cin >> B[0] >> B[1];
-            std::cout << "Input 3rd point's coordinates (between space): ";
-            std::cin >> C[0] >> C[1];
+            inputCoordinates(A, B, C);
             break;
         default:
             std::cout << "Input error: error in input" << std::endl;
         }
 
         while (!back_flag) {
-            std::cout << "Methods: 1) get square," << std::endl
-                << "2) get perimeter, " << std::endl
-                << "3) get type of triangle," << std::endl
-                << "4) compare with another triangle by square," << std::endl
-                << "5) back to first menu," << std::endl
-                << "0) exit," << std::endl
-                << "Input: ";
-            std::cin >> user_choosen;
-
+            userPick2(&user_choosen);
             switch (user_choosen) {
             case 0:
                 exit_flag = true;
                 break;
             case 1:
                 if (_inputType == bySides) {
-                    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
-                        p = (AB + BC + AC) * 0.5;
-                        S = sqrt(p * (p - AB) * (p - BC) * (p - AC));
-                        std::cout << "Square of triangle: S = " << S << std::endl;
-                    }
-                    else {
-                        std::cout << "Input error: triangle dont exist" << std::endl;
-                    }
+                    calcSqSides(AB, AC, BC);
                 }
                 else if (_inputType == byBaseAndHeight) {
-                    if (AB > AC && AB > BC && h != 0)
-                        S = 0.5 * AB * h;
-                    else if (AC > AB && AC > BC && h != 0)
-                        S = 0.5 * AC * h;
-                    else if (BC > AC && BC > AB && h != 0)
-                        S = 0.5 * BC * h;
-                    else {
-                        std::cout << "Input error: triangle dont exist" << std::endl;
-                    }
+                    calcSqBaseH(AB, h);
                 }
                 else if (_inputType == byPointsCoords) {
-                    S = 0.5 * abs(((A[0] - C[0]) * (B[1] - C[1])) - ((B[0] - C[0]) * (A[1] - C[1])));
+                    calcSqPoints(A, B, C);
                 }
                 system("pause");
                 system("cls");
                 break;
             case 2:
                 if (_inputType == bySides) {
-                    p = AB + BC + BC;
+                    calcPrSides(AB, AC, BC);
                 }
                 else if (_inputType == byBaseAndHeight) {
-                    std::cout << "Input error: we couldn't find perimeter by your parameters" << std::endl;
+                    calcPrBaseH();
                 }
                 else if (_inputType == byPointsCoords) {
-                    int ABx = B[0] - A[0];
-                    int ABy = B[1] - A[1];
-                    int ACx = C[0] - A[0];
-                    int ACy = C[1] - A[1];
-                    int BCx = C[0] - B[0];
-                    int BCy = C[1] - B[1];
-                    p = sqrt(pow(ABx, 2) + pow(ABy, 2)) + sqrt(pow(ACx, 2) + pow(ACy, 2)) + sqrt(pow(BCx, 2) + pow(BCy, 2));
+                    calcPrPoints(A, B, C);
                 }
                 break;
             case 3:
-                if (AB == AC == BC)
-                    std::cout << "Triangle is equilateral" << std::endl;
-                else if (AB == AC || AC == BC || AB == BC)
-                    std::cout << "Triangle is isosceles" << std::endl;
-                else if (pow(AB, 2) + pow(AC, 2) == pow(BC, 2) || pow(BC, 2) + pow(AC, 2) == pow(AB, 2) || pow(AB, 2) + pow(BC, 2) == pow(AC, 2))
-                    std::cout << "Triangle is isosceles" << std::endl;
-                else
-                    std::cout << "Triangle is scalene" << std::endl;
+                triangleType(AB, AC, BC);
                 break;
             case 4:
-                int S1, S2;
-                if (S1 == S2)
-                    std::cout << "Triangles have equal square" << std::endl;
-                else 
-                    std::cout << "Triangles' squares are not equal" << std::endl;
+                triangleCompare(S1, S2);
                 break;
             case 5:
                 back_flag = true;
@@ -213,19 +308,7 @@ int main() {
             }
 
             if (back_flag || exit_flag) {
-                system("cls");
-                std::cout << "Last parameters: ";
-                if (_inputType == bySides) {
-                    std::cout << AB << "x" << BC << "x" << AC << std::endl;
-                }
-                else if (_inputType == byBaseAndHeight) {
-                    std::cout << "a = " << AB << ", h = " << h << std::endl;
-                }
-                else if (_inputType == byPointsCoords) {
-                    std::cout << "(" << A[0] << ", " << A[1] << ")" << ", ";
-                    std::cout << "(" << B[0] << ", " << B[1] << ")" << ", ";
-                    std::cout << "(" << C[0] << ", " << C[1] << ")" << std::endl;
-                }
+                menuExitOrBack(AB, AC, BC, A, B, C, h, _inputType);
             }
             break;
         }
